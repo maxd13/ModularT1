56f9ff529bf736508bc0ae75d167c0e8a2a2ebd4
Luiz Carlos R Viana <luizcarlosrvn424@gmail.com>
2018-08-31 20:00:33 -0300

    Fizemos o deployment e modificamos as instrucoes de deployment do relat instal para maior claridade. Tambem modificamos o testatudo.bat para trabalhar com o Release do projeto.

--- Scripts/testatudo.bat
+++ Scripts/testatudo.bat
@@ -3,7 +3,7 @@ REM executa os testes da pasta de scripts do projeto ModularT1
 
 del estatisticas.estat
 
-..\Debug\ModularT1.exe      /sTesteTotal        /lTesteTotal        /aestatisticas
+..\Release\ModularT1.exe      /sTesteTotal        /lTesteTotal        /aestatisticas
 
 
 REM para visualizar as estatisiticas use o programa exbestat da pasta simples, com a sintaxe: exbestat /eestatisticas
